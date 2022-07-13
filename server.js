4111bd70282b4ee77486ad0378cd5814f65af516
Matthew Kaus <matthewkausfox@gmail.com>
2022-07-12 22:24:30 -0600

    added public folder and routes to server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'))
 
 const { animals } = require('./data/animals');
 
@@ -56,7 +57,7 @@ function validateAnimal(animal) {
     if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
         return false;
     }
-    return ture;
+    return true;
 }
 
 function createNewAnimal(body, animalsArray) {
@@ -91,6 +92,22 @@ app.post('/api/animals', (req, res) => {
         const animal = createNewAnimal(req.body, animals);
         res.json(animal);
     }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(3001, () => {
